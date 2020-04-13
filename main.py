9a3b6d43f401a1e3eb8d2fb5873ae824aa57d037
wide9510 <49491361+wide9510@users.noreply.github.com>
2020-04-12 18:13:53 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -7,28 +7,39 @@ from datetime import datetime
 
 current_day = datetime.today()
 sim_start_date = "{}-{}-{}".format(current_day.year, current_day.month, current_day.day)     # simulating a solar system starting from this date
-sim_duration = 2 * 365            # (int) simulation duration in days
-m_earth = 5.9722e24 / 1.98847e30  # Mass of Earth relative to mass of the sun
-m_moon = 7.3477e22 / 1.98847e30
+sim_duration = 10 * 365            # Running the simulation for 10 years, can be adjusted
+
+
+def scale(metric): # planets
+    return metric / 10 ** 5
+
+def scale2(metric):
+    return metric / 10 ** 6 # orbits
+
+def scale3(metric):
+    return metric / 1500000  # sun
+
 
 class Object:                   # define the objects: the Sun, Earth, Mercury, etc
     def __init__(self, name, rad, color, r, v):
-        self.name = name
-        self.r    = np.array(r, dtype=np.float)
-        self.v    = np.array(v, dtype=np.float)
-        self.xs = []
-        self.ys = []
-        self.plot = ax.scatter(r[0], r[1], color=color, s=rad**2, edgecolors=None, zorder=10)
-        self.line, = ax.plot([], [], color=color, linewidth=1.4)
+        self.name   = name
+        self.r      = np.array(r, dtype = np.float)
+        self.v      = np.array(v, dtype = np.float)
+        self.xs     = []
+        self.ys     = []
+        self.plot   = ax.scatter(r[0], r[1], color = color, s = rad ** 2, edgecolors = None, zorder = 10, label = "{}".format(name))
+        self.line,  = ax.plot([], [], color = color, linewidth = 1.4)
 
 class SolarSystem:
     def __init__(self, thesun):
         self.thesun = thesun
         self.planets = []
         self.time = None
-        self.timestamp = ax.text(.03, .94, 'Date: ', color='w', transform=ax.transAxes, fontsize='x-large')
+        self.timestamp = ax.text(.03, .94, 'Date: ', color = 'w', transform = ax.transAxes, fontsize = 'x-large')
+
     def add_planet(self, planet):
         self.planets.append(planet)
+
     def evolve(self):           # evolve the trajectories
         dt = 1.0
         self.time += dt
@@ -36,7 +47,7 @@ class SolarSystem:
         lines = []
         for p in self.planets:
             p.r += p.v * dt
-            acc = -2.959e-4 * p.r / np.sum(p.r**2)**(3./2)  # in units of AU/day^2
+            acc = -2.959e-4 * p.r / np.sum(p.r ** 2) ** (3. / 2)  # in units of AU/day^2
             p.v += acc * dt
             p.xs.append(p.r[0])
             p.ys.append(p.r[1])
@@ -45,27 +56,31 @@ class SolarSystem:
             p.line.set_ydata(p.ys)
             plots.append(p.plot)
             lines.append(p.line)
-        self.timestamp.set_text('Date: ' + Time(self.time, format='jd', out_subfmt='date').iso)
+        self.timestamp.set_text('Date: ' + Time(self.time, format = 'jd', out_subfmt = 'date').iso)
         return plots + lines + [self.timestamp]
 
 plt.style.use('dark_background')
-fig = plt.figure(figsize=[6, 6])
-ax = plt.axes([0., 0., 1., 1.], xlim=(-1.8, 1.8), ylim=(-1.8, 1.8))
+fig = plt.figure(figsize = [8, 8])
+ax = plt.axes([0., 0., 1., 1.], xlim = (-5, 5), ylim = (-5, 5))
 ax.set_aspect('equal')
 ax.axis('off')
-ss = SolarSystem(Object("Sun", 28, 'red', [0, 0, 0], [0, 0, 0]))
+ss = SolarSystem(Object("Sun", scale3(1.3927E6), 'yellow', [0, 0, 0], [0, 0, 0]))
 ss.time = Time(sim_start_date).jd
-colors = ['gray', 'orange', 'blue', 'chocolate']
-sizes = [0.38, 0.95, 1., 0.53]
-names = ['Mercury', 'Venus', 'Earth', 'Mars']
-texty = [.47, .73, 1, 1.5]
-for i, nasaid in enumerate([1, 2, 3, 4]):  # The 1st, 2nd, 3rd, 4th planet in solar system
-    obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type='id').vectors()
-    ss.add_planet(Object(nasaid, 20 * sizes[i], colors[i], 
-                         [np.double(obj[xi]) for xi in ['x', 'y', 'z']], 
+colors = ['gray', 'orange', 'blue', 'red', 'navajowhite', 'goldenrod', 'mediumaquamarine', 'steelblue', 'lightgray'] # colors of planets in order
+sizes = [scale(4879.4), scale(12104), scale(12742), scale(6779), scale(139820), scale(116460), scale(50724), scale(49244), scale(2376.6)] # Diameters compared to Earth
+names = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+texty = [scale2(7.031E7), scale2(1.092E8), scale2(21.496E8), scale2(2.2739E8), scale2(7.779E8) , scale2(1.4331E9) , scale2(2.87527E9), scale2(4.50439E9), scale2(5.8942E9)] # AU distance of planets from sun
+
+for i, nasaid in enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9]):  # Going through the nine planets in the SolarSystem
+    obj = Horizons(id = nasaid, location = "@sun", epochs = ss.time, id_type = 'id').vectors()
+    ss.add_planet(Object(nasaid, 20 * sizes[i], colors[i],
+                         [np.double(obj[xi]) for xi in ['x', 'y', 'z']],
                          [np.double(obj[vxi]) for vxi in ['vx', 'vy', 'vz']]))
-    ax.text(0, - (texty[i] + 0.1), names[i], color=colors[i], zorder=1000, ha='center', fontsize='large')
+    ax.legend(loc = 'upper right')
+
 def animate(i):
     return ss.evolve()
-ani = animation.FuncAnimation(fig, animate, repeat=False, frames=sim_duration, blit=True, interval=20,)
+
+ani = animation.FuncAnimation(fig, animate, repeat = False, frames = sim_duration, blit = True, interval = 20,)
+
 plt.show()
